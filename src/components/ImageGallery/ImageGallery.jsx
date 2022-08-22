7c329e7c04c79494cbac2fed9d8d225a8acac21e
Vittalina <vitalina.alleksandrovna@gmail.com>
2022-08-22 15:37:58 +0300

    commit

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 // import { nanoid } from 'nanoid';
 // import { ListItem, Button } from 'components/Contacts/Contacts.styled';
 
-const ImageGallery = ({}) => {
+const ImageGallery = () => {
   return (
     <ul className="gallery">{/* <!-- Набор <li> с изображениями --> */}</ul>
   );
