8b6adcd8fd761b4290cb7471f8618c89309887d0
Vittalina <vitalina.alleksandrovna@gmail.com>
2022-08-26 17:16:44 +0300

    commit

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,8 +1,8 @@
-const response = (searchQueryPicture, perPage, page) => {
-  const BASE_URL = 'https://pixabay.com/api/';
-  const MY_API_KEY = '28384939-76d0db34094acd1949cd365d2';
+const BASE_URL = 'https://pixabay.com/api/';
+const MY_API_KEY = '28384939-76d0db34094acd1949cd365d2';
 
-  fetch(
+const response = (searchQueryPicture, perPage, page) => {
+  return fetch(
     `${BASE_URL}?key=${MY_API_KEY}&q=${searchQueryPicture}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${perPage}&page=${page}`
   ).then(response => response.json());
 };
